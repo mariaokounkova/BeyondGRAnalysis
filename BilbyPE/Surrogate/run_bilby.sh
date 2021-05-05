2e7df08b87348f3dc2aba41579429f93f8747d50
Maria Okounkova <maria.okounkova@ldas-pcdev5.ligo.caltech.edu>
2021-05-04 17:08:25 -0700

    run_bilby.sh: update LAL environment var

--- BilbyPE/Surrogate/run_bilby.sh
+++ BilbyPE/Surrogate/run_bilby.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
+export LAL_DATA_PATH=/home/maria.okounkova/.local/lib/python3.6/site-packages/gwsurrogate/surrogate_downloads/:$LAL_DATA_PATH
 #source /home/thomas.callister/virtualEnvs/emcee/bin/activate
 python3 /home/maria.okounkova/BeyondGRAnalysis/BilbyPE/Surrogate/fast_tutorial.py
