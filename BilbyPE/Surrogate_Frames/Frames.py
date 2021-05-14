04fbb862ed4b6eeb900045f06007bd0c580e0e71
Maria Okounkova <maria.okounkova@ldas-pcdev5.ligo.caltech.edu>
2021-05-14 12:09:48 -0700

    Frames.py: Read in our own psd files

--- BilbyPE/Surrogate_Frames/Frames.py
+++ BilbyPE/Surrogate_Frames/Frames.py
@@ -57,97 +57,86 @@ for det in interferometer_names:
                             channel = det + ":LDAS_STRAIN")
 
     ifo.set_strain_data_from_gwpy_timeseries(data)
+    
+    ## Read in our own psd
+    
+    psd_file = '/home/maria.okounkova/BeyondGRAnalysis/BilbyPE/Surrogate_Frames/aLIGOZeroDetHighPower-PSD.txt'
+    psd_frequencies_value, psd_value = np.loadtxt(psd_file, comments="#",usecols=([0,1]),unpack=True)
 
-    logger.info("Downloading psd data for ifo {}".format(det))
-    ## Read in the fake psd data (all zeros in our case)
-    psd_data = gwutils.read_frame_file(file_name = \
-                    "/home/maria.okounkova/BeyondGRAnalysis/BilbyPE/" + \
-                    "Surrogate_Frames/Frames_PSD/" \
-                                    + det + ".gwf", \
-                          start_time = psd_start_time, \
-                          end_time = psd_end_time, \
-                          buffer_time = 4.0, \
-                          channel = det + ":LDAS_STRAIN")
-
-
-    psd_alpha = 2 * roll_off / duration  # shape parameter of tukey window
-    psd = psd_data.psd(
-        fftlength=duration,
-        overlap=0,
-        window=("tukey", psd_alpha),
-        method="median")
     ifo.power_spectral_density = bilby.gw.detector.PowerSpectralDensity(
-        frequency_array=psd.frequencies.value, psd_array=psd.value)
+        frequency_array=psd_frequencies_value, psd_array=psd_value)
+    
     ifo_list.append(ifo)
+
 
 logger.info("Saving data plots to {}".format(outdir))
 bilby.core.utils.check_directory_exists_and_if_not_mkdir(outdir)
 ifo_list.plot_data(outdir=outdir, label=label)
 
-######################
-#### Priors ##########
-######################
+# ######################
+# #### Priors ##########
+# ######################
 
-##### CHOOSE PRIOR FILE
-##### DEFAULT PRIOR FILES: GW150914.prior, binary_black_holes.prior,
-##### Needs to specify path if you want to use any other prior file.
-prior = bilby.gw.prior.BBHPriorDict(filename='Frame.prior')
+# ##### CHOOSE PRIOR FILE
+# ##### DEFAULT PRIOR FILES: GW150914.prior, binary_black_holes.prior,
+# ##### Needs to specify path if you want to use any other prior file.
+# prior = bilby.gw.prior.BBHPriorDict(filename='Frame.prior')
 
-######################
-#### Analysis ########
-######################
+# ######################
+# #### Analysis ########
+# ######################
 
-sampling_frequency = 2048.  # same at which the data is stored ## Masha changed from 4096
+# sampling_frequency = 2048.  # same at which the data is stored ## Masha changed from 4096
 
-# Set up waveform arguments
-waveform_arguments = dict(waveform_approximant='NRSur7dq4',
-                          reference_frequency=25., minimum_frequency=25.)
-#waveform_arguments = {
-#    'waveform_approximant': 'IMRPhenomPv2',
-#    'reference_frequency': 50  # most sensitive frequency
-#}
+# # Set up waveform arguments
+# waveform_arguments = dict(waveform_approximant='NRSur7dq4',
+#                           reference_frequency=25., minimum_frequency=25.)
+# #waveform_arguments = {
+# #    'waveform_approximant': 'IMRPhenomPv2',
+# #    'reference_frequency': 50  # most sensitive frequency
+# #}
 
-# Waveform generator 
-waveform_generator = bilby.gw.WaveformGenerator(
-    duration=duration, sampling_frequency=sampling_frequency,
-    #time_domain_source_model=bilby.gw.source.lal_binary_black_hole,
-    frequency_domain_source_model=bilby.gw.source.lal_binary_black_hole,
-    parameter_conversion=bilby.gw.conversion.convert_to_lal_binary_black_hole_parameters,
-    waveform_arguments=waveform_arguments)
+# # Waveform generator 
+# waveform_generator = bilby.gw.WaveformGenerator(
+#     duration=duration, sampling_frequency=sampling_frequency,
+#     #time_domain_source_model=bilby.gw.source.lal_binary_black_hole,
+#     frequency_domain_source_model=bilby.gw.source.lal_binary_black_hole,
+#     parameter_conversion=bilby.gw.conversion.convert_to_lal_binary_black_hole_parameters,
+#     waveform_arguments=waveform_arguments)
 
-#waveform_generator = bilby.gw.WaveformGenerator(
-#    parameter_conversion=conversion,
-#    frequency_domain_source_model=bilby.gw.source.lal_binary_black_hole,
-#    waveform_arguments=waveform_arguments)
+# #waveform_generator = bilby.gw.WaveformGenerator(
+# #    parameter_conversion=conversion,
+# #    frequency_domain_source_model=bilby.gw.source.lal_binary_black_hole,
+# #    waveform_arguments=waveform_arguments)
 
-# Likelihood
-likelihood = bilby.gw.GravitationalWaveTransient(
-    interferometers=ifo_list, waveform_generator=waveform_generator)
+# # Likelihood
+# likelihood = bilby.gw.GravitationalWaveTransient(
+#     interferometers=ifo_list, waveform_generator=waveform_generator)
 
-#likelihood = bilby.gw.likelihood.GravitationalWaveTransient(
-#    interferometers=ifo_list, waveform_generator=waveform_generator,
-#    priors=prior, time_marginalization=False, distance_marginalization=False,
-#    phase_marginalization=False)
+# #likelihood = bilby.gw.likelihood.GravitationalWaveTransient(
+# #    interferometers=ifo_list, waveform_generator=waveform_generator,
+# #    priors=prior, time_marginalization=False, distance_marginalization=False,
+# #    phase_marginalization=False)
 
-# Run sampler 
-## TODO: Get injection parameters in as arg injection_parameters=injection_parameters
-result = bilby.run_sampler(
-    likelihood=likelihood, priors=prior, sampler='dynesty', npoints=1000,
-    outdir=outdir, label=label)
-
-# Implemented Samplers:
-# LIST OF AVAILABLE SAMPLERS: Run -> bilby.sampler.implemented_samplers
-#npoints = 512  # number of live points for the nested sampler
-#n_steps = 100  # min number of steps before proposing a new live point,
-# defaults `ndim * 10`
-#sampler = 'dynesty'
-# Different samplers can have different additional kwargs,
-# visit https://lscsoft.docs.ligo.org/bilby/samplers.html for details.
-
+# # Run sampler 
+# ## TODO: Get injection parameters in as arg injection_parameters=injection_parameters
 # result = bilby.run_sampler(
-#     likelihood, prior, outdir=outdir, label=label,
-#     sampler=sampler, nlive=npoints, use_ratio=False,
-#     walks=n_steps, n_check_point=10000, check_point_plot=True,
-#     conversion_function=bilby.gw.conversion.generate_all_bbh_parameters)
+#     likelihood=likelihood, priors=prior, sampler='dynesty', npoints=1000,
+#     outdir=outdir, label=label)
 
-result.plot_corner()
+# # Implemented Samplers:
+# # LIST OF AVAILABLE SAMPLERS: Run -> bilby.sampler.implemented_samplers
+# #npoints = 512  # number of live points for the nested sampler
+# #n_steps = 100  # min number of steps before proposing a new live point,
+# # defaults `ndim * 10`
+# #sampler = 'dynesty'
+# # Different samplers can have different additional kwargs,
+# # visit https://lscsoft.docs.ligo.org/bilby/samplers.html for details.
+
+# # result = bilby.run_sampler(
+# #     likelihood, prior, outdir=outdir, label=label,
+# #     sampler=sampler, nlive=npoints, use_ratio=False,
+# #     walks=n_steps, n_check_point=10000, check_point_plot=True,
+# #     conversion_function=bilby.gw.conversion.generate_all_bbh_parameters)
+
+# result.plot_corner()
