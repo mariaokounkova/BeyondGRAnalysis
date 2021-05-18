42d78b5b1604206303f93f6a2d494122c409d8c5
Maria Okounkova <maria.okounkova@ldas-pcdev5.ligo.caltech.edu>
2021-05-18 11:32:32 -0700

    Surrogate_FramesProcs: bilby files for multiple processors

---
+++ BilbyPE/Surrogate_FramesProcs/run_bilby.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+export LAL_DATA_PATH=/home/maria.okounkova/.local/lib/python3.6/site-packages/gwsurrogate/surrogate_downloads/:$LAL_DATA_PATH
+python3 /home/maria.okounkova/BeyondGRAnalysis/BilbyPE/Surrogate_FramesProcs/Frames.py
